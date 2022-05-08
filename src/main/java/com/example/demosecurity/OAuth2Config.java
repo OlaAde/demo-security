06967444f8fbcdd217cdb45b5f714a833d51a3e7
Emmanuel <emmanuel.oladipo@oi-devs.com>
2022-05-08 23:52:33 +0300

    part-1

--- src/main/java/com/example/demosecurity/OAuth2Config.java
+++ src/main/java/com/example/demosecurity/OAuth2Config.java
@@ -1,49 +1,17 @@
 package com.example.demosecurity;
 
-import com.azure.spring.aad.webapi.AADResourceServerWebSecurityConfigurerAdapter;
 import com.azure.spring.aad.webapp.AADWebSecurityConfigurerAdapter;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class OAuth2Config {
+public class OAuth2Config extends AADWebSecurityConfigurerAdapter {
 
-    private static final String APPROLE_ROLE_1 = "APPROLE_ROLE_1";
-    private static final String APPROLE_ROLE_2 = "APPROLE_ROLE_2";
-
-    @Order(1)
-    @Configuration
-    public static class ApiWebSecurityConfigurationAdapter extends AADResourceServerWebSecurityConfigurerAdapter {
-        protected void configure(HttpSecurity http) throws Exception {
-            super.configure(http);
-            http.antMatcher("/api/**").authorizeRequests().anyRequest().hasAnyAuthority(APPROLE_ROLE_1, APPROLE_ROLE_2).and().oauth2Login();
-        }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests().anyRequest().authenticated().and().oauth2Login();
     }
-
-    @Configuration
-    public static class HtmlWebSecurityConfigurerAdapter extends AADWebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            super.configure(http);
-            http
-                    .csrf()
-                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                    .requireCsrfProtectionMatcher(httpServletRequest -> false)
-                    .and()
-                    .authorizeRequests()
-                    .antMatchers("/actuator/health", "/public/*").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                    .oauth2Login();
-        }
-    }
-
 }
-
